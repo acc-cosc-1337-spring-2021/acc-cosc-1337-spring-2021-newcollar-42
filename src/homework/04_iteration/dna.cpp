43c24beff8d6f6591c69bb36aad1b7ebe3367f00
newcollar-42 <alison.claggett@g.austincc.edu>
2021-03-26 20:27:04 -0500

    Homework 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,7 @@
 #include "dna.h"
+using namespace std;
+#include<string>
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,15 +9,29 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna) 
 
+{   
+    double g_or_c_count = 0;
+        for (int i = 0; i < dna.length(); i++)
+        {
+            if (dna[i] == 'G' || dna[i] == 'C')
+            {
+                g_or_c_count++;
+            }
+        }
+        double result =  g_or_c_count / dna.length();
+        return result;
+        
+}
 
-
-
-/*
-Write code for function get_reverse_string that
-accepts a string parameter and returns a string reversed.
-*/
-
+string get_reverse_string(string dna)
+{
+   string reverseDNA = "";
+   for (int i = dna.length() - 1; i >= 0; i--)
+       reverseDNA += dna[i];
+   return reverseDNA;
+}
 
 
 /*
@@ -28,3 +45,50 @@ c. return string
 
 */
 
+string get_dna_complement(string dna) {
+
+
+    string comp = get_reverse_string(dna);
+
+    string complement = "";
+
+  
+
+    for (int i = 0; i < comp.length(); i++) {
+
+        
+
+        if (comp[i] == 'A') {
+
+            complement +='T';
+
+        }
+
+       
+
+        else if(comp[i] == 'T') {
+
+            complement += 'A';
+
+        }
+
+    
+        else if(comp[i] == 'G') {
+
+            complement += 'C';
+
+        }
+
+
+        else if(comp[i] == 'C') {
+
+            complement += 'G';
+
+        }
+
+    }
+
+
+    return complement;
+
+}
--- test/homework_test/04_iteration_test/04_iteration_tests.cpp
+++ test/homework_test/04_iteration_test/04_iteration_tests.cpp
@@ -1,7 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "dna.cpp"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
+}
+
+
+
+TEST_CASE("Testing get_gc_content function") 
+{
+	REQUIRE(get_gc_content("AGCTATAG") == 0.375);
+	REQUIRE(get_gc_content("CGCTATAG") == 0.50);
+}
+
+TEST_CASE("Testing reverse_string function") 
+{
+	REQUIRE(get_reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(get_reverse_string("CGCTATAG") == "GATATCGC");
+}
+
+TEST_CASE("Testing get_dna_complement function") 
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
 }
 
